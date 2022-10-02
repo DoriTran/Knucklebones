1674fb52a35a53feb06b9ea27c4a7ae9c9093735
DoriTran <tranquocdong308@gmail.com>
2022-10-02 21:49:29 +0700

    Finish home page

--- src/Component/BGMPlayer/BGMPlayer.js
+++ src/Component/BGMPlayer/BGMPlayer.js
@@ -5,14 +5,24 @@ import Knucklebones_mp3 from "./Knucklebones.mp3"
 import { useEffect, useRef } from "react"
 
 const BMGPlayer = () => {
-    const audioRef = useRef()
+    const audioRef = useRef(null)
+
     useEffect(() => {
-        audioRef.current.load();
-        audioRef.current.play();
-    }, [])
+        function handleClickOutside(event) {
+            if (audioRef.current && !audioRef.current.contains(event.target)) {
+                audioRef.current.play();
+            }
+        }
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [audioRef]);
 
     return (             
-    <audio autoPlay ref={audioRef} >
+    <audio ref={audioRef} loop>
         <source src={Knucklebones_mp3} type="audio/mp3" />
         Your browser does not support the audio element.
     </audio> );
--- src/Component/HomePage/HomeBackground/HomeBackground.js
+++ src/Component/HomePage/HomeBackground/HomeBackground.js
@@ -1,23 +1,52 @@
 // Resources
-import video from './HomeBackVideo.mp4'
+import eyeGif from './Eye.gif'
+import noeyeGif from './NoEye.gif'
+import sloganGif from './Slogan.gif'
+import threeDices from './ThreeDices.png'
+import spinBones from './SpinBones.png'
+import homeButton from './HomeButton.png'
 
 // SCSS settings
 import "./HomeBackground.scss"
 
+// Material ui
+import Button from '@mui/material/Button';
+
 // React
-import { useRef, useEffect } from 'react'
+import { useState } from 'react'
 
 const HomeBackground = () => {
-    const videoRef = useRef(null)
+    const [isFirstClicked, setClicked] = useState(false)
 
-    useEffect(() => {
+    const [onHover, setHover] = useState(1)
 
-    }, []);
+    const hoverCSSbutton = {backgroundImage: `url(${homeButton})`, color: '#f5ecd3'}
+    const unhoverCSSbutton = {color: '#5a5a5a'}
 
-    return ( 
-    <video className="home-background" ref={videoRef} autoPlay loop muted>
-        <source src={video} type="video/mp4"/>
-    </video> );
+    return (
+        <div className="home-background" onClick={() => setClicked(true)}>
+            <div className="home-center">
+                <img className="home-three-dices" src={threeDices} alt=''/>
+                <div className="home-title">KNUCKLEBONES</div>
+                <img className={isFirstClicked ? "home-spin-bones" : "home-bones"} src={spinBones} alt=''/>
+                <img className="home-three-dices button-home-spacing" src={sloganGif} alt=''/>
+                <Button className="ribbon-button" 
+                    style={onHover === 1 ? hoverCSSbutton : unhoverCSSbutton} 
+                    onMouseEnter={() => setHover(1)}>Play</Button>
+                <Button className="ribbon-button" 
+                    style={onHover === 2 ? hoverCSSbutton : unhoverCSSbutton} 
+                    onMouseEnter={() => setHover(2)}>Find room</Button>
+                <Button className="ribbon-button" 
+                    style={onHover === 3 ? hoverCSSbutton : unhoverCSSbutton} 
+                    onMouseEnter={() => setHover(3)}>How to play</Button>
+            </div>
+
+            <img className="homegif-topleft" src={eyeGif} alt=""/>
+            <img className="homegif-topright" src={noeyeGif} alt=""/>
+            <img className="homegif-botleft" src={noeyeGif} alt=""/>
+            <img className="homegif-botright" src={eyeGif} alt=""/>
+        </div>
+ );
 }
  
 export default HomeBackground;
